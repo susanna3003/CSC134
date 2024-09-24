dfaf919fc46c1eb414efe6f7f5984d113ba198ef
susu <113452531+susanna3003@users.noreply.github.com>
2024-09-24 16:22:42 +0000

    finisjhing

--- M3/M3LAB1_Quayle.cpp
+++ M3/M3LAB1_Quayle.cpp
@@ -10,6 +10,10 @@ using namespace std;
 int main()
 {
     int user_choice;
+    string name;
+    cout << "Hello! Welcome aboard the ship! What is your name?" << endl;
+    cin >> name;
+    cout << "\nAlright " << name << ", are you ready to brave these waters?\n\nOh it doesn't matter, you're aboard and we are set off to the seas!" << endl;
 
     cout << "       __|__               You wake up to the sound of thunder crashing around you.\n"
         << "       |     |              The ship is violently rocking as waves pound against its hull.\n"
@@ -28,11 +32,28 @@ int main()
         << " ~  4. a floating barrel             ~ \n"
         << " ~ ~ ~ ~ ~~~ ~ ~ ~ ~ ~~~ ~~~ ~ ~ ~ ~ ~ " << endl;
 
-    cout << "\nWhich will you choose?";
+    cout << "\nWhich will you choose?" << endl;
     cin >> user_choice;
 
     if (user_choice == 1)
     {
+        char yes_no;
+        cout << "You have chosen the rusty plank! Are you ready to fight for your survival?" << endl;
+        cin >> yes_no;
+        cout << "The ocean does not wait for readiness!" << endl;
+        cout << "~ ~ ~~~";
+        cout << "     ~ ~~~     ~";
+        cout << "~~~ ~  ~       ~";
+        cout << "~      ~~~     ~~";
+        cout << "Oh dear, it seems a shark has chewed off your legs while they dangled off your rusty plank and you bled out to death. RIP" << endl;
 
+        cout << "     _______   \n";
+        cout << "    /      \\  \n";
+        cout << "   /        \\ \n";
+        cout << "  |   R.I.P   | \n";
+        cout << "  |           |    \n";
+        cout << "  |  " << name << "  | \n";
+        cout << "  |           |    \n";
+        cout << "  |___________|    \n";
     }
 }
