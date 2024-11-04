fd2c34a63a265b0daabb89ddaf95c658d52abe88
susu <113452531+susanna3003@users.noreply.github.com>
2024-11-04 16:33:22 +0000

    go
    Co-authored-by: RAVINGMADROSE <RAVINGMADROSE@users.noreply.github.com>

--- M5/M5HW1_Quayle.cpp
+++ M5/M5HW1_Quayle.cpp
@@ -12,9 +12,12 @@ using namespace std;
 
 // declare funcs
 void avg_rainfall();
-void calc_hyperrectangle();
+void calc_hyperrectangle(double width, double length, double height);
 void get_roman_num();
-
+double area_circle();
+double area_rectangle();
+double area_triangle();
+void distance_traveled(double speed, double hours);
 
 int main()
 {
@@ -45,66 +48,87 @@ int main()
         {
             case 1:
             {
-                // Q1
-                // INPUT
+                // avg rainfall
+                // this is where we'll calculate average rainfall
+                string month1, month2, month3;
+                double rfall1, rfall2, rfall3, avg;
+                cout << "Enter month: ";
                 
-
-                // PROCESS
-                
-
-                // OUTPUT
 
                 break;
             }
             
             case 2:
             {
-                // Q2
-                // INPUT
-
-
-                // PROCESS
-
-
-                // OUTPUT
-
-
+                // 3d hyperrectangle
+                cout << "Calculate a 3DD Hyperrectangle (i.e. just a cube)" << endl;
+                double width, length, height, volume;
+                cout << "Enter width: ";
+                cin >> width;
+                cout << "Enter length: ";
+                cin >> length;
+                cout << "Enter height: ";
+                cin >> height;
+                volume = calc_hyperrectangle(width, length, height);
                 break;
             }
 
             case 3:
             {
-                // Q3
-                // INPUT
-                
-
-                // PROCESS
-                
-
-                // OUTPUT
-
-
+                // roman numeral
+                int digit;
+                cout << "Get a roman numeral" << endl;
+                cout << "Enter a number (1-10): ";
+                cin >> digit;
+                get_roman_num(digit);
                 break;
             }
 
             case 4:
             {
-                // Q4
-                // INPUT
-
-
-                // PROCESS
-
-
-                //OUTPUT
-
-
+                // Geometry Calculator
+                cout << "Welcome to the Geometry Calculator!" << endl;
+                cout << "1. Calculate the Area of a Circle" << endl;
+                cout << "2. Calculate the Area of a Rectangle" << endl;
+                cout << "3. Calculate the Area of a Triangle" << endl;
+                cout << "4. Quit" << endl;
+                cout << "Enter your choice (1-4): ";
                 break;
             }
 
             case 5:
             {
-
+                // distance traveled
+                double speed, hours;
+                bool speed_positive = true;
+                bool plus_one_hours = true;
+                while (speed_positive == true)
+                {
+                    cout << "What is the speed you are traveling at? (in mph) ";
+                    cin >> speed;
+                    if (speed > 0)
+                    {
+                        speed_positive = true;
+                    }
+                    else if (speed <= 0)
+                    {
+                        speed_positive = false;
+                    }
+                }
+                while (plus_one_hours == true)
+                {
+                    cout << "How many hours are you traveling? ";
+                    cin >> hours;
+                    if (speed > 1)
+                    {
+                        plus_one_hours = true;
+                    }
+                    else if (speed <= 1)
+                    {
+                        plus_one_hours = false;
+                    }
+                }
+                distance_traveled();
                 break;
             }
 
@@ -121,5 +145,101 @@ int main()
                 break;
             }
         }
+    }
+}
+
+void get_roman_num(digit)
+{
+    switch(digit)
+    {
+            case 1:
+            {
+                cout << "The Roman numeral version of " << digit << " is I" << endl;
+                break;
+            }
+            
+            case 2:
+            {
+                cout << "The Roman numeral version of " << digit << " is II" << endl;
+                break;
+            }
+
+            case 3:
+            {
+                cout << "The Roman numeral version of " << digit << " is III" << endl;
+                break;
+            }
+
+            case 4:
+            {
+                cout << "The Roman numeral version of " << digit << " is IV" << endl;
+                break;
+            }
+
+            case 5:
+            {
+                cout << "The Roman numeral version of " << digit << " is V" << endl;
+                break;
+            }
+
+            case 6:
+            {
+                cout << "The Roman numeral version of " << digit << " is VI" << endl;
+                break;
+            }
+
+            case 7:
+            {
+                cout << "The Roman numeral version of " << digit << " is VII" << endl;
+                break;
+            }
+            
+            case 8:
+            {
+                cout << "The Roman numeral version of " << digit << " is VIII" << endl;
+                break;
+            }
+
+            case 9:
+            {
+                cout << "The Roman numeral version of " << digit << " is IX" << endl;
+                break;
+            }
+
+            case 10:
+            {
+                cout << "The Roman numeral version of " << digit << " is X" << endl;
+                break;
+            }
+        
+    }
+}
+
+void calc_hyperrectangle(double width, double length, double height)
+{
+    double volume = width * length * height;
+    return volume;
+}
+
+double area_circle() {
+    
+}
+
+double area_rectangle() {
+
+}
+
+double area_triangle() {
+
+}
+
+void distance_traveled(double speed, double hours)
+{
+    cout << "Hour\tDistance Traveled" << endl;
+    cout << "------------------------" << endl;
+    for (int i = 1; i < hours; i++)
+    {
+        double distance = speed * hours;
+        cout << i << "\t" << distance << endl;
     }
 }
