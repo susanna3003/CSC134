8bbf13f37af8a48267b158c4f6cb2398e2da1ffb
susu <113452531+susanna3003@users.noreply.github.com>
2024-11-04 15:16:21 +0000

    added arearec

---
+++ M5/AreaRectangle.cpp
@@ -0,0 +1,39 @@
+// M5LAB2
+// Header goes here
+
+#include <iostream>
+using namespace std;
+
+// Declare (Write the prototypes for)
+// the getLength,
+// getWidth, getArea, and displayData
+// functions here.
+
+int main()
+{
+	// This program calculates the area of a rectangle.
+	// TODO: fix any syntax errors
+	
+   double length,    // The rectangle's length
+          width,     // The rectangle's width
+          area;      // The rectangle's area
+          
+   // Get the rectangle's length.
+   length = getLength();
+   
+   // Get the rectangle's width.
+   width = getWidth()
+   
+   // Get the rectangle's area.
+   are = getArea();
+   
+   // Display the rectangle's data.
+   displayData(length, width, area);
+          
+   return 0;
+}
+
+//***************************************************
+// TODO: write the getLength, getWidth, getArea,    *
+// and displayData functions below.                 *
+//***************************************************
